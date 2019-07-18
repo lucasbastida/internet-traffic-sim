0c3ae2a9f62ba2fb41167e3cddf2d0f693ea7fd6
user-lucas <48137388+user-lucas@users.noreply.github.com>
2019-07-18 19:59:03 -0300

    added problem statement

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Router{
+
+};
+
+class Terminal{
+
+};
+
+class Page{
+
+};
+
+class Packet{
+
+};
+
+class Admin{
+
+};
+
+
+
+int main(){
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
