92156aedcf2cc86ea12d6b15a48945563dfecf6b
user-lucas <48137388+user-lucas@users.noreply.github.com>
2019-07-22 00:12:42 -0300

    incomplete. Added bandwidth and modifiable weight.

--- DirectedEdge.h
+++ DirectedEdge.h
@@ -11,6 +11,8 @@ private:
     int w;
     double weight;
 
+    double bandwidth;
+
 public:
     DirectedEdge();
     DirectedEdge(int v, int w, double weight);
@@ -19,17 +21,31 @@ public:
     int to() { return w; }
     double getWeight() { return weight; }
     std::string toString() { return (std::to_string(v) + "->" + std::to_string(w) + " " + std::to_string(weight)); }
+    void updateWeight(int queue);
 };
 
 DirectedEdge::DirectedEdge() : v(0), w(0), weight(0.0)
 {
 }
 
-DirectedEdge::DirectedEdge(int v, int w, double weight) : v(v), w(w), weight(weight)
+DirectedEdge::DirectedEdge(int v, int w, double weight) : v(v), w(w), weight(weight), bandwidth(weight)
 {
     if (v < 0 || w < 0)
     {
         throw std::invalid_argument("received negative value");
+    }
+}
+
+void DirectedEdge::updateWeight(int queue)
+{
+    if (queue == 0)
+    {
+        weight = bandwidth;
+    }
+    
+    else
+    {
+        weight = (queue/bandwidth);
     }
 }
 
