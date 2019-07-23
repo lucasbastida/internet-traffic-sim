f3396b15a830c43dc226472de555aa199b4ffe06
user-lucas <48137388+user-lucas@users.noreply.github.com>
2019-07-22 21:13:05 -0300

    created headers for classes in main file. replaced litieral 0 for named variable

--- main.cpp
+++ main.cpp
@@ -11,14 +11,6 @@ class Terminal
 {
 };
 
-class Page
-{
-};
-
-class Packet
-{
-};
-
 int main()
 {
     ifstream ifs("tinyEWD.txt");
@@ -27,13 +19,14 @@ int main()
     cout << graph.toString() << endl;
 
     cout << "DijkstraSP:" << endl;
-    Dijkstra sp(graph, 0);
+    int s = 3;
+    Dijkstra sp(graph, s);
     for (int t = 0; t < graph.getV(); t++)
     {
         if (sp.hasPathTo(t))
         {
             stack<DirectedEdge> s1 = sp.pathTo(t);
-            cout << 0 << " to " << t << " ("<< sp.distTo(t) <<") : ";
+            cout << s << " to " << t << " ("<< sp.distTo(t) <<") : ";
             string path = "";
             while (!s1.empty())
             {
