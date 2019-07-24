21ea9e442baeeaafb1305f9d54c0a7b4526971b8
user-lucas <48137388+user-lucas@users.noreply.github.com>
2019-07-24 16:14:03 -0300

    updated main to work with new files

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,16 @@
 
 #include "Admin.h"
 #include <stack>
+#include <chrono>
+#include <thread>
 
 using namespace std;
+
+using namespace std::this_thread; // sleep_for, sleep_until
+using namespace std::chrono;      // nanoseconds, system_clock, seconds
+
+// sleep_for(nanoseconds(10));
+// sleep_until(system_clock::now() + seconds(1));
 
 int main()
 {
@@ -14,23 +22,22 @@ int main()
 
     cout << "Testing cycle" << endl;
 
-    admin->cycle();
-
     for (size_t i = 0; i < 10; i++)
     {
-        if (i%2 == 0)
+        if (i % 2 == 0)
         {
             admin->updateRouteTable();
         }
-        
-        admin->cycleNoPage();
+
+        admin->cycle();
 
         for (int i = 0; i < admin->G->getV(); i++)
         {
             cout << admin->G->nodes[i].toString() << endl;
         }
-        
+
         cout << admin->G->toString() << endl;
+        // sleep_for(seconds(3));
     }
     return 0;
 }
