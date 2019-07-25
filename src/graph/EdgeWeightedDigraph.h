4b57ab98fa886d510d9b8be075edac1ccfd62519
user-lucas <48137388+user-lucas@users.noreply.github.com>
2019-07-25 01:09:09 -0300

    Fixed memory leak

--- src/graph/EdgeWeightedDigraph.h
+++ src/graph/EdgeWeightedDigraph.h
@@ -15,18 +15,18 @@ private:
     int E;
 
     void validateVertex(int v);
+
 public:
-    Router* nodes;
-    
-    EdgeWeightedDigraph(std::istream& ist);
+    Router *nodes;
+
+    EdgeWeightedDigraph(std::istream &ist);
     EdgeWeightedDigraph(int V);
     ~EdgeWeightedDigraph();
     int getE() { return E; }
     int getV() { return V; }
-    void addEdge(DirectedEdge *e);
+    void addEdge(DirectedEdge e);
     std::list<DirectedEdge> adjList(int v);
     std::string toString();
-
 };
 
 EdgeWeightedDigraph::EdgeWeightedDigraph(int V) : V(V)
@@ -35,37 +35,35 @@ EdgeWeightedDigraph::EdgeWeightedDigraph(int V) : V(V)
     {
         throw std::invalid_argument("|V| in a Digraph must be nonnegative");
     }
-    
+
     E = 0;
-    
+
     nodes = new Router[V];
+    std::cout << "loaded " << V << " routers in graph" << std::endl;
 }
 
-EdgeWeightedDigraph::EdgeWeightedDigraph(std::istream& ist) : V(0),E(0)
+EdgeWeightedDigraph::EdgeWeightedDigraph(std::istream &ist) : V(0), E(0)
 {
-	ist >> V >> E;
+    ist >> V >> E;
     if (V < 0)
     {
         throw std::invalid_argument("|V| in a Digraph must be nonnegative");
     }
 
     nodes = new Router[V];
+    std::cout << "loaded " << V << " routers in graph" << std::endl;
 
-	for (int i = 0; i < E; i++)
-	{
-		int v, w;
-		double dWeight;
-		ist >> v;
-		ist >> w;
-		ist >> dWeight;
-        DirectedEdge * edge = new DirectedEdge(v, w, dWeight);
-		this->addEdge(edge);
-		E--;
-	}
-}
-
-EdgeWeightedDigraph::~EdgeWeightedDigraph()
-{
+    for (int i = 0; i < E; i++)
+    {
+        int v, w;
+        double weight;
+        ist >> v;
+        ist >> w;
+        ist >> weight;
+        DirectedEdge edge(v,w,weight);
+        addEdge(edge);
+        E--;
+    }
 }
 
 void EdgeWeightedDigraph::validateVertex(int v)
@@ -74,15 +72,15 @@ void EdgeWeightedDigraph::validateVertex(int v)
         throw std::invalid_argument("vertex " + std::to_string(v) + " is not between 0 and " + std::to_string((V - 1)));
 }
 
-void EdgeWeightedDigraph::addEdge(DirectedEdge* e)
+void EdgeWeightedDigraph::addEdge(DirectedEdge e)
 {
-    int v = e->from();
-    int w = e->to();
+    int v = e.from();
+    int w = e.to();
     validateVertex(v);
     validateVertex(w);
 
-    nodes[v].adj.push_back(*e);
-
+    nodes[v].adj.push_back(e);
+    // std::cout << "ADDED EDGE: " << e->toString() << std::endl;
     E++;
 }
 
@@ -96,10 +94,10 @@ std::list<DirectedEdge> EdgeWeightedDigraph::adjList(int v)
 std::string EdgeWeightedDigraph::toString()
 {
     std::string s = "";
-    s.append(std::to_string(V) + " " + std::to_string(E) + "\n");
+    s.append(std::to_string(V) + " routers " + std::to_string(E) + " edges\n");
     for (int v = 0; v < V; v++)
     {
-        s.append(std::to_string(v) + ": ");
+        s.append("router ip:" + std::to_string(v) + " : ");
         for (std::list<DirectedEdge>::iterator it = nodes[v].adj.begin(); it != nodes[v].adj.end(); ++it)
         {
             s.append(it->toString() + " ");
@@ -109,4 +107,11 @@ std::string EdgeWeightedDigraph::toString()
     return s;
 }
 
+EdgeWeightedDigraph::~EdgeWeightedDigraph()
+{
+    if (nodes)
+    {
+        delete[] nodes;
+    }
+}
 #endif
